ea65f997ce522cd06fdadc37b8c1a1e89619a6b6
arisygdc <arisycso@gmail.com>
2021-12-27 18:34:28 +0700

    database structure

---
+++ database/migration/000001_init_schema.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS tipe_sensor;
+DROP TABLE IF EXISTS informasi_sensor;
+DROP TABLE IF EXISTS monitoring_location;
+DROP TABLE IF EXISTS sensors;
+DROP TABLE IF EXISTS input_sensor;
---
+++ database/migration/000001_init_schema.up.sql
@@ -0,0 +1,43 @@
+CREATE TABLE tipe_sensor (
+    id SERIAL NOT NULL PRIMARY KEY,
+    tipe VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE informasi_sensor (
+    id SERIAL NOT NULL PRIMARY KEY,
+    status BOOLEAN NOT NULL,
+    identity VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE monitoring_location (
+    id SERIAL NOT NULL PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL UNIQUE,
+    provinsi VARCHAR(50) NOT NULL,
+    kecamatan VARCHAR(50) NOT NULL,
+    desa VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE sensors (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    tipe_sensor_id INT NOT NULL,
+    mon_loc_id INT NOT NULL,
+    ditempatkan_pada TIMESTAMP NOT NULL,
+
+    CONSTRAINT tipe_sensor
+        FOREIGN KEY (tipe_sensor_id)
+        REFERENCES tipe_sensor(id)
+
+    CONSTRAINT monitoring_location
+        FOREIGN KEY (mon_loc_id)
+        REFERENCES monitoring_location(id)
+);
+
+CREATE TABLE input_sensor (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    sensor_id INT NOT NULL,
+    data FLOAT NOT NULL,
+
+    CONSTRAINT sensors
+        FOREIGN KEY (sensor_id)
+        REFERENCES sensors(id)
+);
