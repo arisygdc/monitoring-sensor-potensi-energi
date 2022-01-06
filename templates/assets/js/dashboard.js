45eab1036b76a1092f154be35f316c73360dc8b3
arisygdc <arisycso@gmail.com>
2022-01-06 22:15:12 +0700

    monitoring chart

--- templates/assets/js/dashboard.js
+++ templates/assets/js/dashboard.js
@@ -1,55 +1,93 @@
 /* globals Chart:false, feather:false */
 
 (function () {
-    'use strict'
-  
-    feather.replace({ 'aria-hidden': 'true' })
-  
-    // Graphs
-    var ctx = document.getElementById('myChart')
-    var metrics = [
-      10000,
-      21345,
-      18483,
-      24003,
-      23489,
-      24092,
-      12034
-    ]
-    // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: metrics,
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false
-            }
-          }]
-        },
-        legend: {
-          display: false
+  'use strict'
+  feather.replace({ 'aria-hidden': 'true' })
+})()
+
+var api = "http://127.0.0.1:8080/api/v1";
+var localurl = "http://127.0.0.1:5500/templates";
+
+function PlaceChart(id) {
+  axios.get(api+"/monitoring/"+id).then(
+    (response) => {
+        var result = response.data.metrics
+        var labels = [], metrics = [], len = result.length
+        
+        if (len > 30) {
+          len = 30
+        } 
+        
+        for(var i=0; i < len; i++) {
+          metrics[i] = result[i].data
+          labels[i] = result[i].dibuat_pada
         }
-      }
-    })
-  })()
-  
+        
+        // Graphs
+        var ctx = document.getElementById('myChart')
+      
+        // eslint-disable-next-line no-unused-vars
+        var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: labels,
+            datasets: [{
+              data: metrics,
+              lineTension: 0,
+              backgroundColor: 'transparent',
+              borderColor: '#007bff',
+              borderWidth: 4,
+              pointBackgroundColor: '#007bff'
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: false
+                }
+              }]
+            },
+            legend: {
+              display: false
+            }
+          }
+        })
+  });
+}
+
+function GetSensors() {
+  axios.get(api+"/sensors").then(
+  (response) => {
+      var ctx = document.getElementById('sensors')
+      var result = response.data.sensors;
+      var text = ""
+      var no = 1
+      result.forEach(element => {
+          text += "<tr><td>"
+              +no
+              +"</td><td><a href=\""+localurl+"/monitoring.html?id="+element.id.Int64+"\">"
+              +element.tipe
+              +"</a></td><td>"
+              +element.ditempatkan_pada.Time
+              +"</td><td>"
+              +element.provinsi+", "+ element.kecamatan +", "+ element.desa
+              +"</td><td>"
+              +element.status.Bool
+              +"</td></tr>"
+          no++
+      });
+      ctx.innerHTML = text;
+  },
+  (error) => {
+      console.log(error);
+  }
+  );
+}
+
+function getMetrics(id) {
+  axios.get(api+"/monitoring/"+id).then(
+    (response) => {
+        return response.data.metrics;
+  });
+}
